1e87a1f23746d0b427954dd0f6cfd7e288e75763
Julia <schmid.julia96@gmail.com>
2023-05-17 15:55:33 +0200

    wip: shapley values

--- tests/test_predictor.py
+++ tests/test_predictor.py
@@ -1,3 +1,5 @@
+import matplotlib.figure
+
 from tests.fixtures import *
 import numpy as np
 
@@ -82,6 +84,23 @@ class TestPredictor:
         with pytest.raises(PyPythiaException):
             predictor.predict(query)
 
+    def test_plot_shapley_values(self, predictor):
+        query = {
+            "num_patterns/num_taxa": 0.0,
+            "num_sites/num_taxa": 0.0,
+            "num_patterns/num_sites": 0.0,
+            "proportion_gaps": 0.0,
+            "proportion_invariant": 0.0,
+            "entropy": 0.0,
+            "pattern_entropy": 0.0,
+            "bollback": 0.0,
+            "avg_rfdist_parsimony": 0.0,
+            "proportion_unique_topos_parsimony": 0.0,
+        }
+
+        fig = predictor.plot_shapley_values(query)
+        assert isinstance(fig, matplotlib.figure.Figure)
+
 
 class TestBackwardsSklearnCompatibility:
     def test_predict(self, sklearn_predictor):
@@ -102,3 +121,18 @@ class TestBackwardsSklearnCompatibility:
         # but the prediction should be a float between 0.0 and 1.0
         assert isinstance(prediction, float)
         assert 0.0 <= prediction <= 1.0
+
+    def test_plot_shapley_values(self, sklearn_predictor):
+        query = {
+            "num_patterns/num_taxa": 0.0,
+            "num_sites/num_taxa": 0.0,
+            "proportion_gaps": 0.0,
+            "proportion_invariant": 0.0,
+            "entropy": 0.0,
+            "bollback": 0.0,
+            "avg_rfdist_parsimony": 0.0,
+            "proportion_unique_topos_parsimony": 0.0,
+        }
+
+        fig = sklearn_predictor.plot_shapley_values(query)
+        assert isinstance(fig, matplotlib.figure.Figure)
