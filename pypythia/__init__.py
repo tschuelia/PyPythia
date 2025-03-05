6d8fe77203e26a786ac50b953fc839d50800fdcc
Julia <schmid.julia96@gmail.com>
2025-03-05 11:57:04 +0100

    rename package and adjust version retrieval for pypi

--- pypythia/__init__.py
+++ pypythia/__init__.py
@@ -4,3 +4,12 @@ try:
     __version__ = importlib.metadata.distribution(__name__).version
 except Exception:
     __version__ = "unknown"
+
+# Required if the package was installed via PyPi...
+if __version__ == "unknown":
+    try:
+        from importlib.metadata import version
+
+        __version__ = version("PythiaPhyloPredictor")
+    except Exception:
+        pass
